4b1d470c6ee87a48819825d94eba9549f4d29342
SomyaBaran <somyabaranwal6664@gmail.com>
2026-06-20 12:12:12 +0530

    init

--- docker_compose/src/index.ts
+++ docker_compose/src/index.ts
@@ -1,18 +1,32 @@
 import express from "express";
+import { PrismaClient } from "./generated/prisma/client.js";
+import { PrismaPg } from "@prisma/adapter-pg";
 
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+const prisma = new PrismaClient({ adapter });
 const app = express();
+
+
 app.use(express.json());
 
-app.post("/", (req, res) => {
+app.post("/", async (req, res) => {
+    await prisma.user.create({
+        data: {
+            username: Math.random().toString(),
+            password: Math.random().toString(),
+
+        }
+    })
     res.json({
         message: "post endpoint"
     });
 });
-app.get("/", (req, res) => {
-    res.json({
-        message: "get endpoint"
-    });
+app.get("/", async (req, res) => {
+    const data = await prisma.user.findMany();
+    res.json({ data });
 });
+
+
 app.listen(3000, () => {
     console.log("post is running on 3000");
 });
