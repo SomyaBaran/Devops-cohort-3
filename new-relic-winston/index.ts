e42f6e7b7377d922d9512704e23097cbcf2aa3b2
SomyaBaran <somyabaranwal6664@gmail.com>
2026-08-02 14:37:32 +0530

    testing winston logs

--- new-relic-winston/index.ts
+++ new-relic-winston/index.ts
@@ -1,21 +1,23 @@
+// require("newrelic");
 import winston from "winston";
 
 const logger = winston.createLogger({
   level: "info",
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.prettyPrint()
-  ),
+  // format: winston.format.combine(
+  //   winston.format.timestamp(),
+  //   winston.format.prettyPrint()
+  // ),
+  // format: winston.format.simple(),
+  format: winston.format.json(),
   transports: [
-    new winston.transports.File({
-      filename: "error.log",
-      level: "error",
-    }),
-    new winston.transports.File({
-      filename: "combined.log",
-    }),
+    new winston.transports.File({ filename: "error.log", level: "error" }),
+    new winston.transports.File({ filename: "combined.log" }),
+    new winston.transports.Console()
   ],
 });
 
 logger.error("Hello, world!");
 logger.info("Hello, world!");
+logger.debug("developer log");
+logger.warn("warning log");
+logger.error("error log");
